97ae9c887ef153423c46abcbeabd35d3673b7193
Mohamed Amine <133964099+ruinedm@users.noreply.github.com>
2024-12-29 15:01:31 +0000

    mariadb works :)

---
+++ mariadb/setup_mariadb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sed -i "s/^bind-address\s*=\s*.*/bind-address = 0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
+
+echo "Starting up MariaDB..."
+service mariadb start
+
+echo "Waiting for MariaDB to start..."
+sleep 2
+
+
+echo "Creating database and user..."
+
+mysql -e "CREATE DATABASE IF NOT EXISTS wp_db;"
+mysql -e "CREATE USER IF NOT EXISTS 'ruined'@'%' IDENTIFIED BY '123';"
+mysql -e "GRANT ALL PRIVILEGES ON wp_db.* TO 'ruined'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
+
+echo "MariaDB setup completed."
