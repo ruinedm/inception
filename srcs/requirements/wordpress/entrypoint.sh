23177b7fb81787ad45cb33c4719f09b30fdda00d
Mohamed Amine <133964099+ruinedm@users.noreply.github.com>
2024-12-29 22:16:36 +0000

    Working on WORDPRESS :)

---
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+./wait-for-it.sh mariadb:3306 --timeout=30 --strict -- echo "MariaDB is up and running"
+
+
+# we will be using volumes to store the wordpress files, so we need to check if the files are already there
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+    echo "Downloading and setting up WordPress..."
+    wp core download --path=/var/www/html --allow-root
+    wp config create --dbname=wp_db --dbuser=ruined --dbpass=123 --dbhost=mariadb --allow-root --path=/var/www/html
+    wp core install --url=http://localhost --title="ruined" --admin_user=ruined --admin_password=123 --allow-root --path=/var/www/html
+    wp user create user user@gmail.com --role=author --user_pass=123 --allow-root --path=/var/www/html
+else
+    echo "WordPress already configured"
+fi
+php-fpm7 -F
---
+++ srcs/requirements/wordpress/setup_wordpress.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Downloading and setting up wp-cli..."
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp 
+
+echo "Downloading wait-for-it.sh..."
+curl -o wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
+chmod +x wait-for-it.sh
+
+if [ ! -d "/var/www/html" ]; then
+    mkdir -p /var/www/html
+fi
+echo "Downloading and setting up WordPress..."
+wp core download --path=/var/www/html --allow-root
