8896a304a0882217e8fd583a322b7e6bcec55f12
Ruined <dynamicamine@gmail.com>
2024-12-30 15:35:47 -0500

    better wp setup

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -3,5 +3,15 @@
 
 ./wait-for-it.sh mariadb:3306 --timeout=30 --strict -- echo "MariaDB is up and running"
 
+
+if [ ! -d "/var/www/html/wp-config.php" ]; then
+    echo "Downloading and setting up WordPress..."
+    cd /var/www/html
+    wp config create --dbname=wp_db --dbuser=ruined --dbpass=123 --dbhost=mariadb --allow-root 
+    wp core install --url=http://localhost --title="ruined" --admin_user=ruined --admin_password=123 --admin_email="dynamicamine@gmail.com" --allow-root 
+    wp user create user user@gmail.com --role=author --user_pass=123 --allow-root 
+fi
+
+
 echo "Starting php-fpm..."
 php-fpm8.2 -F
