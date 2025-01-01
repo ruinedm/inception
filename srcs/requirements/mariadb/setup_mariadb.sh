638ccd0e78eb528c81adfd4e028448c0e56419a9
Ruined <dynamicamine@gmail.com>
2025-01-01 17:31:50 -0500

    volumes with paths

--- srcs/requirements/mariadb/setup_mariadb.sh
+++ srcs/requirements/mariadb/setup_mariadb.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+echo "Downloading wait-for-it.sh..."
+curl -o wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
+chmod +x wait-for-it.sh
 
+echo "Setting up mariadb to listen from any port..."
 sed -i "s/^bind-address\s*=\s*.*/bind-address = 0.0.0.0/" /etc/mysql/mariadb.conf.d/50-server.cnf
 echo "Starting up MariaDB..."
 service mariadb start
--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -4,16 +4,16 @@
 ./wait-for-it.sh mariadb:3306 --timeout=30 --strict -- echo "MariaDB is up and running"
 
 
-if [ ! -f "/var/www/html/.wp_installed" ]; then
+if [ ! -f "/var/www/html/wp-config.php" ]; then
     echo "Downloading and setting up WordPress..."
     cd /var/www/html
     wp core download --allow-root
     wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=123 --dbhost=mariadb --allow-root 
     wp core install --url=http://localhost --title=$WEBSITE_NAME --admin_user=$ADMIN_USERNAME --admin_password=123 --admin_email=$ADMIN_MAIL --allow-root 
     wp user create $USER_USERNAME $USER_MAIL --role=author --user_pass=123 --allow-root 
-    touch .wp_installed
 fi
 
+cat /var/www/html/.wp_installed
 
 echo "Starting php-fpm..."
 php-fpm8.2 -F
