fb2efa0dc413b4462757c0496f9526d2d750c58e
Ruined <dynamicamine@gmail.com>
2025-01-12 16:42:47 -0500

    ok

--- srcs/bonus/ftp/tools/setup_ftp.sh
+++ srcs/bonus/ftp/tools/setup_ftp.sh
@@ -1,11 +1,12 @@
 #/bin/sh
-useradd -m ftpsecure && echo "ftpsecure:123" | chpasswd
-
-
+useradd -m $FTP_USER && echo "$FTP_USER:$FTP_PASS" | chpasswd
+chown -R $FTP_USER:$FTP_USER /home/$FTP_USER
+chmod 755 /home/$FTP_USER
+chmod u+w /home/$FTP_USER
 sed -i 's/^#\?anonymous_enable=.*/anonymous_enable=NO/' /etc/vsftpd.conf
 sed -i 's/^#\?local_enable=.*/local_enable=YES/' /etc/vsftpd.conf
 sed -i 's/^#\?write_enable=.*/write_enable=YES/' /etc/vsftpd.conf
-sed -i 's/^#\?nopriv_user=.*/nopriv_user=ftpsecure/' /etc/vsftpd.conf
+sed -i "s/^#\?nopriv_user=.*/nopriv_user=$FTP_USER/" /etc/vsftpd.conf
 sed -i 's/^#\?xferlog_enable=.*/xferlog_enable=YES/' /etc/vsftpd.conf
 
 
